84669cf4bbf6701768cec1613bf7c3723afc5ce4
lythanh00 <123355879+lythanh00@users.noreply.github.com>
2024-04-05 22:23:34 +0700

    Create frontend and build home page
    Create frontend and build home page

---
+++ Food-Ordering/frontend-food-ordering/src/App.js
@@ -0,0 +1,19 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { Navbar } from "./component/Navbar/Navbar";
+import { ThemeProvider } from "@emotion/react";
+import { darkTheme } from "./Theme/DarkTheme";
+import { CssBaseline } from "@mui/material";
+import Home from "./component/Home/Home";
+
+function App() {
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <Navbar />
+      <Home />
+    </ThemeProvider>
+  );
+}
+
+export default App;
