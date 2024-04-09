8c1bacb983eb72fa72f0cc4b6a964f0b0a74a275
lythanh00 <123355879+lythanh00@users.noreply.github.com>
2024-04-09 21:38:03 +0700

    build profile page
    build profile page

--- frontend-food-ordering/src/component/Navbar/Navbar.jsx
+++ frontend-food-ordering/src/component/Navbar/Navbar.jsx
@@ -4,10 +4,12 @@ import SearchIcon from '@mui/icons-material/Search';
 import { pink } from '@mui/material/colors';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import './Navbar.css';
+import Box from '@mui/material/Box';
 
 export const Navbar = () => {
   return (
-    <div className='px-5 z-50 py-[.8rem] bg-[#e91e63] lg:px-20 flex justify-between'>
+    <Box 
+    className='px-5 sticky top-0 z-50 py-[.8rem] bg-[#e91e63] lg:px-20 flex justify-between'>
       
         <div className='lg:mr-10 cursor-pointer flex items-center space-x-4'>
             <li className='logo font-semibold text-gray-300 text-2xl'>
@@ -36,7 +38,7 @@ export const Navbar = () => {
         </div>
 
       </div>
-    </div>
+    </Box>
   )
 }
 
